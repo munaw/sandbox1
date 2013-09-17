022e7ca15d5366e02d40c4d78d553988cb5f1cbd
munaw <mamunawa@gmail.com>
2013-09-17 17:07:18 -0400

    Added ref soln to the parens problem

--- src/main/java/recurse/Parentheses.java
+++ src/main/java/recurse/Parentheses.java
@@ -47,6 +47,43 @@ public class Parentheses {
 		}
 		return retlist;
 	}
+	
+	/** Ref soln **/
+	
+	    public static void printPar(int l, int r, char[] str, int count) {
+	        System.out.format(" **IN** l: %d r: %d count: %d str: %s\n", l, r, count, new String(str, 0, count));
+	        
+	        if (l < 0 || r < l) {
+	            System.out.println("discard -> " + new String(str, 0, count));
+	            return; // invalid state
+	        }
+	        if (l == 0 && r == 0) {
+	            System.out.println(str); // found one, so print it
+	        } else {
+	
+	            if (l > 0) { // try a left paren, if there are some available
+	                str[count] = '(';
+	                printPar(l - 1, r, str, count + 1);
+	            }
+	
+	            if (r > l) { // try a right paren, if there’s a matching left
+	                str[count] = ')';
+	                printPar(l, r - 1, str, count + 1);
+	            }
+	
+	        }
+	        System.out.format("**OUT** l: %d r: %d count: %d str: %s\n", l, r, count, new String(str, 0, count));
+	    }
+
+	    public static void printPar(int count) {
+	
+	        char[] str = new char[count * 2];
+	
+	        printPar(count, count, str, 0);
+	    }
+    
+	/** end Ref soln **/
+	
 	public static void main(String[] args) {
 		List<String> combs = parentheses(12);
 		for (String s: combs){
